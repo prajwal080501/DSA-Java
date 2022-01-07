0eadf28c1f4a2cfb97690634d409bce0980f0dca
SANEDROID123 <62328357+prajwal080501@users.noreply.github.com>
2022-01-07 17:11:37 +0530

    Linear Search Algorithim

---
+++ DSA/LinearSearch.java
@@ -0,0 +1,51 @@
+package DSA;
+
+//Linear Search Algorithm
+
+import java.util.Scanner;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        int s;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array");
+        s = sc.nextInt();
+        int[] arr = new int[s];
+        accept(arr, s);
+        display(arr, s);
+        int key;
+        System.out.println("Enter a key element to search in array: ");
+        key = sc.nextInt();
+        linearSearch(arr, s, key);
+    }
+
+    static void accept(int arr[], int size) {
+        Scanner in = new Scanner(System.in);
+        for (int i = 0; i < size; i++) {
+            arr[i] = in.nextInt();
+        }
+    }
+
+    static void display(int arr[], int size){
+        for(int i = 0; i< size; i++){
+            System.out.println(arr[i]);
+        }
+    }
+
+    static void linearSearch(int arr[], int size, int key){
+        int comparisonCount = 0;
+        for (int i = 0; i < size; i++) {
+            if (arr[i] == key) {
+                System.out.println("Element found at index: " + i);
+                break;
+            }
+            comparisonCount++;
+
+        }
+        System.out.println("Number of comparisons: " + comparisonCount);
+        System.out.println("Element not found");
+
+    }
+}
+
+
